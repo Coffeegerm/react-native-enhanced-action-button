ca7cd42ba67b618e33472185541eb2301308a697
David Yarzebinski <dyarzebinski97@gmail.com>
2022-05-29 14:49:34 -0400

    working example added

---
+++ example/MyApp/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
+import {ActionButton} from 'react-native-enhanced-action-button';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={[backgroundStyle, {flex: 1}]}>
+      <ActionButton />
+    </SafeAreaView>
+  );
+};
+
+export default App;
